13795ef3a9a9d0e6e0fa32eed908f7e7d706776e
Bourhane Younes <bourhaneyounes1@gmail.com>
2022-12-14 16:35:22 +0100

    test for add function

---
+++ int/adder_test.go
@@ -0,0 +1,26 @@
+package integers 
+
+import (
+  "fmt"
+  "testing"
+)
+func  TestAdder(t *testing.T){
+  t.Run("simple int test", func(t *testing.T){
+    got := Add(1,2)
+    expected := 3
+
+    assertCodeMessage(t,got,expected)
+  })
+}
+
+func ExampleAdd(){
+  sum := Add(1,1)
+  fmt.Println(sum)
+}
+
+func assertCodeMessage(t testing.TB, got, expected int) {
+  t.Helper()
+  if got != expected {
+    t.Errorf("got %d want %d", got, expected)
+  }
+}
