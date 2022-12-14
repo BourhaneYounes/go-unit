9846f6431f447fda3544832766048d5bdb78a90e
Bourhane Younes <bourhaneyounes1@gmail.com>
2022-12-14 13:22:53 +0100

    refactored and added empty string case

--- hello_test.go
+++ hello_test.go
@@ -3,10 +3,24 @@ package main
 import "testing"
 
 func  TestHello(t *testing.T) {
-  got := hello("Chris")
-  want := "Hello, Chris"
+  t.Run("saying hello to people", func(t *testing.T){
+    got := hello("Chris")
+    want := "Hello, Chris"
 
+    assertCodeMessage(t,got,want)
+  })
+
+  t.Run("say 'hello, world' when an empty string is supplied", func(t *testing.T){
+    got := hello("")
+    want := "Hello, world"
+
+    assertCodeMessage(t,got,want)
+  })
+}
+
+func assertCodeMessage(t testing.TB, got, want string){
+  t.Helper()
   if got != want {
-    t.Errorf("got %q want %q",got,want)
+    t.Errorf("got %q want %q", got, want)
   }
 }
