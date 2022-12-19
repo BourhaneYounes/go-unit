ee96d3ded872cd362861daf8f43bffe97b77b297
Bourhane Younes <bourhaneyounes1@gmail.com>
2022-12-19 14:57:45 +0100

    refactored sum test

--- array_slice/sum_test.go
+++ array_slice/sum_test.go
@@ -3,19 +3,16 @@ package main
 import "testing"
 
 func TestSum(t *testing.T){
+  t.Run("collection of numbers any size(omitted size[slices])", func (t *testing.T){
+    numbers := []int{1,2,3}
 
-  t.Run("simple array test", func(t *testing.T){
-
-    numbers := [5]int{1,2,3,4,5}
-  
     got := Sum(numbers)
-    want := 15
-
+    want := 6
     assertCodeMessage(t,got,want,numbers)
   })
 }
 
-func assertCodeMessage(t testing.TB,got,want int, given [5]int){
+func assertCodeMessage(t testing.TB,got,want int, given []int){
   t.Helper()
   if got != want{
     t.Errorf("got %d want %d given %v", got, want, given)
