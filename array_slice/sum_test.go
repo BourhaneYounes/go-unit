c68f77e5b27634a7180d79e776a8cee8c3f39680
Bourhane Younes <bourhaneyounes1@gmail.com>
2022-12-19 15:26:57 +0100

    added sumAll test

--- array_slice/sum_test.go
+++ array_slice/sum_test.go
@@ -1,6 +1,19 @@
 package main
 
 import "testing"
+import "reflect"
+
+func TestSumAll(t *testing.T){
+  t.Run("simple test variadic functions", func(t *testing.T){
+    a := []int{1,2}
+    b := []int{3,4}
+
+    got := SumAll(a,b)
+    want := []int{3,7}
+
+    assertCodeMessageSumAll(t,got,want,a,b)
+  })
+}
 
 func TestSum(t *testing.T){
   t.Run("collection of numbers any size(omitted size[slices])", func (t *testing.T){
@@ -16,5 +29,12 @@ func assertCodeMessage(t testing.TB,got,want int, given []int){
   t.Helper()
   if got != want{
     t.Errorf("got %d want %d given %v", got, want, given)
+  }
+}
+
+func assertCodeMessageSumAll(t testing.TB, got, want, givenA, givenB []int){
+  t.Helper()
+  if !reflect.DeepEqual(got, want){
+    t.Errorf("got %v want %v given %v and %v", got, want, givenA, givenB)
   }
 }
